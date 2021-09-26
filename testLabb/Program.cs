2036ef3e68a2eb6f1d252ae6294857bb9c027fe5
cynt0 <Robin.Liukkonen@gmail.com>
2021-09-26 22:50:22 +0200

    master

--- testLabb/Program.cs
+++ testLabb/Program.cs
@@ -8,20 +8,19 @@ namespace testLabb
         static void Main(string[] args)
         {
             string print = "29535123p48723487597645723645";
-            BigInteger Summan= 0;
+            BigInteger Summan= 0; //Håller i värdet vid loop
             for (int i = 0; i < print.Length -1; i++)
             {
                 char names = print[i];
-                int index = print.IndexOf(names, i + 1);
+                int index = print.IndexOf(names, i + 1); //Kollar index i sträng
                 int length = index - i + 1;
              
-                if (index != -1)
+                if (index != -1) //Kontrollerar så att index inte är -1. Vid -1 så dyker programmet !VIKIGT!
                 {
-                    string result = print.Substring(i, length);
-                    var split = print.Split(names, length);
-                    IsDigitsOnly(result);
+                    string result = print.Substring(i, length);  //Håller i värdet vid beräkning -> Blir sedan Summan
+                    IsDigitsOnly(result); //Kontrollerar så att det ENDAST är sifforor inom index 
                     
-                    if (!IsDigitsOnly(result))
+                    if (!IsDigitsOnly(result)) //Fall det är en bokstav i indexen, så skippas den loopen.
                     {
                         continue;
                     }
@@ -33,7 +32,7 @@ namespace testLabb
                         Console.ResetColor();
                         Console.Write(print.Substring(index + 1));
                         Console.WriteLine();
-                        Summan += BigInteger.Parse(result);
+                        Summan += BigInteger.Parse(result); 
                     }
                     else if(index == print.Length -1)
                     {
